5142443d1b704daec54adc8c2398eaa9ca0595fd
FarhanFardid <farhanfardid009@gmail.com>
2024-08-15 22:01:17 +0600

    Banner Component updated with responsiveness for smaller device

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import icon2 from "../../assets/Icons/Icon 2.svg";
 const Header = () => {
   return (
     <div
-      className="bg-cover bg-no-repeat bg-center flex items-center p-6 md:p-10"
+      className="bg-cover bg-no-repeat bg-center flex items-center p-6 md:p-12 max-w-full"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="mx-auto text-center">
@@ -11,13 +11,17 @@ const Header = () => {
         <h2 className="text-white font-semibold text-2xl md:text-4xl mb-2 md:mb-4">
           Construction
         </h2>
-        <ul className="flex  justify-center items-center font-medium p-1 md:p-0 md:space-x-3  md:mt-0  text-sm md:text-base text-white">
+        <ul className="flex  justify-center items-center font-medium p-1 md:p-0 md:space-x-3  md:mt-0  text-sm md:text-lg text-white">
           <li>
             <div className="flex items-center text-white">
               <a href="" className="block py-2 ">
                 Home
               </a>
-              <img src={icon2} alt="icon2" className="ps-2 w-6" />
+              <img
+                src={icon2}
+                alt="icon2"
+                className="pe-2 ps-1 md:ps-2 md:pe-0 w-7"
+              />
             </div>
           </li>
           <li>
@@ -25,7 +29,11 @@ const Header = () => {
               <a href="" className="block py-2 ">
                 Industry
               </a>
-              <img src={icon2} alt="icon2" className="ps-2 w-6" />
+              <img
+                src={icon2}
+                alt="icon2"
+                className="pe-2 ps-1 md:ps-2 md:pe-0 w-7"
+              />
             </div>
           </li>
           <li>
